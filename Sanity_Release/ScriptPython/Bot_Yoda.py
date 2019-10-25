1c67d85c237c10f3004c1048d2cbc392fba95fdd
l4rodrigues <43417385+l4rodrigues@users.noreply.github.com>
2019-10-25 14:51:06 -0300

    Cenários novos.

--- Sanity_Release/ScriptPython/Bot_Yoda.py
+++ Sanity_Release/ScriptPython/Bot_Yoda.py
@@ -4,7 +4,7 @@ import requests
 from slackclient import SlackClient
 
 # get the api key from the environment variables
-token = "xoxb-465708053495-463929998784-qYieoeuWcW3IPfARbGWjk0JU"
+token = "xoxb-465708053495-463929998784-7f7QOOcxGuXqMtJ0rUGo6Mfr"
 
 
 # function to send messages to Slack
